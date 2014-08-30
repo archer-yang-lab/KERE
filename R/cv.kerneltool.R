86e1bc6854591cb4d7ad7aaaec6e92d448f63449
Yi Yang <emeryyi@gmail.com>
2014-08-29 23:54:12 -0500

    1. add cv function.

---
+++ R/cv.kerneltool.R
@@ -0,0 +1,35 @@
+cv.kerneltool <- function(x, y, kern, lambda = NULL, 
+	pred.loss = c("misclass", "loss"), nfolds = 5, foldid, qval = 2.0, omega = 0.5, ...) {
+    if (missing(pred.loss)) 
+        pred.loss <- "default" else pred.loss <- match.arg(pred.loss)
+    N <- nrow(x)
+    ###Fit the model once to get dimensions etc of output
+    y <- drop(y)
+    # predict -> coef
+    if (missing(foldid)) 
+        foldid <- sample(rep(seq(nfolds), length = N)) else nfolds <- max(foldid)
+    if (nfolds < 3) 
+        stop("nfolds must be bigger than 3; nfolds=10 recommended")
+    outlist <- as.list(seq(nfolds))
+    ###Now fit the nfold models and store them
+    for (i in seq(nfolds)) {
+        which <- foldid == i
+        y_sub <- y[!which]
+        outlist[[i]] <- kerneltool(x = x[!which, , drop = FALSE], 
+            y = y_sub, kern = kern, lambda = lambda, 
+			qval = qval, omega = omega, ...)
+    }
+    ###What to do depends on the pred.loss and the model fit
+    fun <- paste("cv", class(outlist[[1]])[[2]], sep = ".")
+    cvstuff <- do.call(fun, list(outlist, lambda, x, y, kern, foldid, 
+        pred.loss, qval, omega))
+    cvm <- cvstuff$cvm
+    cvsd <- cvstuff$cvsd
+    cvname <- cvstuff$name
+    out <- list(lambda = lambda, cvm = cvm, cvsd = cvsd, cvupper = cvm + 
+        cvsd, cvlo = cvm - cvsd, nzero = nz, name = cvname, kerneltool.fit = kerneltool.object)
+    lamin <- getmin(lambda, cvm, cvsd)
+    obj <- c(out, as.list(lamin))
+    class(obj) <- "cv.kerneltool"
+    obj
+} 
--- R/utilities.R
+++ R/utilities.R
@@ -10,3 +10,32 @@ err <- function(n, maxit) {
     }
     list(n = n, msg = msg)
 }
+
+
+cvcompute <- function(mat, foldid, nlams) {
+    ###Computes the weighted mean and SD within folds, and
+    #   hence
+    #   the se of the mean
+    nfolds <- max(foldid)
+    outmat <- matrix(NA, nfolds, ncol(mat))
+    good <- matrix(0, nfolds, ncol(mat))
+    mat[is.infinite(mat)] <- NA
+    for (i in seq(nfolds)) {
+        mati <- mat[foldid == i, ]
+        outmat[i, ] <- apply(mati, 2, mean, na.rm = TRUE)
+        good[i, seq(nlams[i])] <- 1
+    }
+    N <- apply(good, 2, sum)
+    list(cvraw = outmat, N = N)
+}
+
+
+hubercls <- function(r, delta) {
+  (1 - r - 0.5 * delta) * (r <= 1 - delta) + 0.5 * (1 - r)^2 / delta * (r <= 1) * (r > 1 - delta)
+} 
+
+
+ercls <- function(r, omega) {
+  abs(omega - (r < 0)) * r^2
+}
+
