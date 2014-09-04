8adb75a82de2cf297a68466cc68a319ecaecf1db
Yi Yang <emeryyi@gmail.com>
2014-09-03 19:24:12 -0500

    1. change the 1/n setting

--- R/utilities.R
+++ R/utilities.R
@@ -29,12 +29,6 @@ cvcompute <- function(mat, foldid, nlams) {
     list(cvraw = outmat, N = N)
 }
 
-
-hubercls <- function(r, delta) {
-  (1 - r - 0.5 * delta) * (r <= 1 - delta) + 0.5 * (1 - r)^2 / delta * (r <= 1) * (r > 1 - delta)
-} 
-
-
 ercls <- function(r, omega) {
   abs(omega - (r < 0)) * r^2
 }
