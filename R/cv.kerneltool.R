478cc1dbbad7aefb43e046130e681c6c86c817b9
Yi Yang <emeryyi@gmail.com>
2014-10-05 21:30:52 -0500

    1. fix some bugs

--- R/cv.kerneltool.R
+++ R/cv.kerneltool.R
@@ -10,32 +10,33 @@ cv.kerneltool <- function(x, y, kern, lambda = NULL,
         foldid <- sample(rep(seq(nfolds), length = N)) else nfolds <- max(foldid)
     if (nfolds < 3) 
         stop("nfolds must be bigger than 3; nfolds=5 recommended")
-    outlist <- as.list(seq(nfolds))
     ###Now fit the nfold models and store them
 	nlams <- double(nfolds)
 	predmat <- matrix(NA, length(y), length(lambda))
     for (i in seq(nfolds)) {
         which <- foldid == i
         y_sub <- y[!which]
-        outlist[[i]] <- kerneltool(x = x[!which, , drop = FALSE], 
+        fit <- kerneltool(x = x[!which, , drop = FALSE], 
             y = y_sub, kern = kern, lambda = lambda, omega = omega, ...)
-        preds <- predict(outlist[[i]], kern, x[!which, ,drop = FALSE], x[which, , drop = FALSE])
-        nlami <- length(outlist[[i]]$lambda)
+        preds <- predict(fit, kern, x[!which, ,drop = FALSE], x[which, , drop = FALSE])
+        nlami <- length(fit$lambda)
         predmat[which, seq(nlami)] <- preds
         nlams[i] <- nlami
     }
-    cvraw <- ercls(y-predmat, omega)
+	cvraw <- ercls(y-predmat, omega)
 	cvob <- cvcompute(cvraw, foldid, nlams)
 	cvraw <- cvob$cvraw
 	N <- cvob$N
-    cvm <- colMeans(cvraw, na.rm = TRUE)
-    cvsd <- sqrt(colMeans(scale(cvraw, cvm, FALSE)^2, na.rm = TRUE)/(N - 1))
-    cvm <- na.omit(cvm)
-    cvsd <- na.omit(cvsd)
-    out <- list(lambda = lambda, cvm = cvm, cvsd = cvsd, cvupper = cvm + 
-        cvsd, cvlo = cvm - cvsd, name = "Expectile Loss")
-    lamin <- getmin(lambda, cvm, cvsd)
-    obj <- c(out, as.list(lamin))
-    class(obj) <- "cv.kerneltool"
-    obj
+	cvm <- colMeans(cvraw, na.rm = TRUE)
+	cvsd <- sqrt(colMeans(scale(cvraw, cvm, FALSE)^2, na.rm = TRUE)/(N - 1))
+	cvm <- na.omit(cvm)
+	cvsd <- na.omit(cvsd)
+	cvm.min <- min(cvm)
+	idmin <- cvm <= cvm.min
+	lambda.min <- max(lambda[idmin])
+	obj <- list(lambda = lambda, cvm = cvm, cvsd = cvsd, cvupper = cvm + 
+	    cvsd, cvlo = cvm - cvsd, name = "Expectile Loss", cvm.min = cvm.min, 
+		lambda.min = lambda.min)
+	class(obj) <- "cv.kerneltool"
+	obj
 } 
